19ba22217aba774aceba8dda79be40bed8fbe102
shalikahmed <122047338+shalikahmed@users.noreply.github.com>
2023-08-25 10:27:46 +0530

    final commit

--- Sustainable-Select-backend/routes/stripe.js
+++ Sustainable-Select-backend/routes/stripe.js
@@ -3,14 +3,37 @@ const stripe = require("stripe")(process.env.STRIPE_KEY)
 
 router.post("/payment",(req,res)=>{
     stripe.charges.create(
-    {
-        source: req.body.tokenId,
-        amount: req.body.amount,
-        currency: "usd"
-    },(stripeError,stripeRes)=>{
-        if(stripeError) {res.status(500).json(stripeError)}
+        {
+            source: req.body.tokenId,
+            amount: req.body.amount,
+            currency: "usd"
+        },(stripeError,stripeRes)=>{
+            if(stripeError) {
+            res.status(500).json(stripeError)}
         else {res.status(200).json(stripeRes)}
     })
 })
 
 module.exports = router
+
+// const router = require("express").Router();
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+// router.post("/payment", (req, res) => {
+//   stripe.charges.create(
+//     {
+//       source: req.body.tokenId,
+//       amount: req.body.amount,
+//       currency: "usd"
+//     },
+//     (stripeError, stripeRes) => {
+//       if (stripeError) {
+//         res.status(500).json(stripeError);
+//       } else {
+//         res.status(200).json(stripeRes);
+//       }
+//     }
+//   );
+// });
+
+// module.exports = router;
